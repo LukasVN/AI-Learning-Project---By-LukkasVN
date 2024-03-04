a39e31deff5f52eb57b7ebe40a48e9dca0d7f470
Lucas Varela Negro <lucasvnbusiness@gmail.com>
2024-03-04 01:15:03 +0100

    Initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    float speed = 2f;
+
+    void Start() {
+        direction = goal.transform.position - transform.position;
+
+    }
+
+    private void LateUpdate() {
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        transform.Translate(velocity);
+    }
+}
