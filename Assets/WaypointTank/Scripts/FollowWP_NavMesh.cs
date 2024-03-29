5a2ca83ef52acf6a53340581401ea90c376c155e
Lucas Varela Negro <lucasvnbusiness@gmail.com>
2024-03-29 23:24:24 +0100

    Added a NavMesh to the WayPoint project and added a script that uses it

---
+++ Assets/WaypointTank/Scripts/FollowWP_NavMesh.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class FollowWP_NavMesh : MonoBehaviour
+{
+    public GameObject wpManager;
+    private GameObject[] wps;
+    private GameObject currentNode;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        currentNode = wps[0];
+
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    public void GoToStart(){
+        // It can be a given transform when using NavMesh
+        agent.SetDestination(wps[0].transform.position);
+    }
+
+    public void GoToHelicopterZone(){
+        agent.SetDestination(wps[10].transform.position);
+    }
+
+    public void GoToBuild(){
+        agent.SetDestination(wps[16].transform.position);
+    }
+
+    public void GoToRocks(){
+        agent.SetDestination(wps[15].transform.position);
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
