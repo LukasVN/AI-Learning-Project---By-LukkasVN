ecdd7f94ec180a60af589b54a5be56f8dd8edb0d
Lucas Varela Negro <lucasvnbusiness@gmail.com>
2024-04-20 13:30:11 +0200

    Added a Spawner of Patients

--- Assets/Goal Driven Behaviour/Scripts/Spawner.cs
+++ Assets/Goal Driven Behaviour/Scripts/Spawner.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject patientprefab;
+    public int numPatients;
+    void Start(){
+        for (int i = 0; i < numPatients; i++){
+            Instantiate(patientprefab, transform.position, Quaternion.identity);
+        }
+
+            Invoke("SpawnPatient",5f);
+
     }
 
-    // Update is called once per frame
+    private void SpawnPatient(){
+        Instantiate(patientprefab, transform.position, Quaternion.identity);
+        Invoke("SpawnPatient",Random.Range(2f,10f));
+    }
     void Update()
     {
         
